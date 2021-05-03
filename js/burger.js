3a4bc17f3ceb9251a67ce49392f156169e8b18b0
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-05-03 02:38:22 +0200

    updating navbar, removed old function, added simple dropdown button

--- js/burger.js
+++ js/burger.js
@@ -1,18 +1,10 @@
 const navSlide = function () {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-  const navSearchContainer = document.querySelector(
-    ".nav-search-social-container"
-  );
+  let burger = document.querySelector(".burger");
+  const nav = document.querySelector(".section-2");
+  const navLinks = document.querySelectorAll(".section-2 li");
 
   burger.addEventListener("click", function () {
     nav.classList.toggle("nav-active");
-    if (navSearchContainer.style.animation) {
-      navSearchContainer.style.animation = "";
-    } else {
-      navSearchContainer.style.animation = `navLinkFade 2s ease forwards`;
-    }
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
@@ -20,9 +12,11 @@ const navSlide = function () {
         link.style.animation = `navLinkFade 0.5s ease forwards ${
           index / 7 + 0.5
         }s`;
+        nav.style.display = "none";
       }
     });
     burger.classList.toggle("toggle");
+    nav.style.display = "flex";
   });
 };
 
