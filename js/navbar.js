4de12956df6e853054e28cf51b164a7435ed4b88
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-05-07 02:10:23 +0200

    fixing nav, media query

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,29 @@
-var prevPosition = window.pageYOffset;
-window.onscroll = function () {
-  var currentPosition = window.pageYOffset;
-  if (prevPosition > currentPosition) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-200px";
-  }
-  prevPosition = currentPosition;
-};
+var menuButton = document.querySelector("#menu-button");
+menuButton.addEventListener("click", showMenu, true);
+
+var link = document.querySelector("#mobile-link");
+link.addEventListener("click", hideMenu, false);
+
+function showMenu(e) {
+  link.classList.add("show");
+
+  document.body.style.overflow = "hidden";
+}
+
+function hideMenu(e) {
+  link.classList.remove("show");
+  e.stopPropagation();
+
+  document.body.style.overflow = "auto";
+}
+
+// var prevPosition = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentPosition = window.pageYOffset;
+//   if (prevPosition > currentPosition) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-200px";
+//   }
+//   prevPosition = currentPosition;
+// };
